6e06f2b87626bbafd31b47961731dc4c08a9a6b3
abhinavp06 <aparasharchd06@gmail.com>
2025-12-21 16:56:12 +0530

    basic surgical quantizer

--- 01_Phase_1_Master_Bit_Crusher/01_Amplitude_Logic/03_MidRise_vs_MidTread_Architectures/implementations/01_surgical_quantizer.cpp
+++ 01_Phase_1_Master_Bit_Crusher/01_Amplitude_Logic/03_MidRise_vs_MidTread_Architectures/implementations/01_surgical_quantizer.cpp
@@ -5,28 +5,33 @@
 #include <cstdlib>
 #include <cmath>
 
-class SurgicalQuantizer {
+class QuantizerEngine {
+
 public:
-	[[nodiscard]] float mid_tread_quanitzer(float input) const noexcept {
-		return std::floor(input * levels + 0.5f) * inv_levels;
+	void process_block(float* buffer, int buffer_size, bool use_mid_tread) {
+		/*
+		* Mid tread ->
+		*	In the case of a mid tread, we want to snap to the closest whole number.
+		*	This is why an offset of 0.5f is required (since std::floor is a down only function)
+		* Mid rise ->
+		*	In the case of a mid rise, we want to avoid zero.
+		*	This is why an offset is added after rounding off so that we never get a true 0.
+		* 
+		* Why not add an offset for mid rise anyways?
+		* -> Because if we do that, we just have a mid tread which is up by 0.5 (assuming we still add 0.5f to the quantized value later)
+		*/
+		const float offset = use_mid_tread ? 0.5f : 0.0f;
+
+		for (int i = 0; i < buffer_size; i++) {
+			float quantized_value = std::floor(buffer[i] * inverted_step_size + offset);
+
+			if (!use_mid_tread) quantized_value += 0.5f;
+
+			buffer[i] = quantized_value * step_size;
+		}
 	}
-	[[nodiscard]] float mid_rise_quantizer(float input) const noexcept {
-		return (std::floor(input * levels) + 0.5f) * inv_levels;
-	}
+
 private:
-	float levels{ 15.0f };  // assuming we had a bit rate of 4
-	float inv_levels{ 1.0f / 15.0f };
+	float step_size{ 1.0f / 32768.0f }; // this is basically "stretching" the 0-1 range to 32768 steps - which gives the true value for a single step.
+	float inverted_step_size{ 32768.0f };
 };
-
-
-int main() {
-	// Assuming a float vector came in as the buffer.
-	// Not going to actually implement the receival of a buffer.
-	std::vector<float> hardware_buffer(BUFFER_SIZE);
-
-	for (int i = 0; i < BUFFER_SIZE; i++) {
-		hardware_buffer[i] = mid_rise_quantizer(hardware_buffer[i]); // for mid rise quantizer
-		//hardware_buffer[i] = mid_tread_quantizer(hardware_buffer[i]); // for mid tread quantizer
-	}
-
-}
